df5c3f69175f960926575008a3bfddc379bb5f43
so2emma <osoemmanuel1969@gmail.com>
2024-06-28 11:12:53 +0100

    setup for ticket functionality

--- models/ticket.js
+++ models/ticket.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-
+const Ticket = require('../utils/ticketTypes')
 const ticketSchema = mongoose.Schema({
-    type: { type: String, required: true },
-    price: { type: Number, required: true },
-    event: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
-    buyer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-},{timestamps: true})
+    type: {type: String, enum: Object.values(Ticket), default: Ticket.GENERAL_TICKET},
+    price: {type: Number, required: true},
+    event: {type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true},
+    buyer: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+}, {timestamps: true})
 
 module.exports = mongoose.model('Ticket', ticketSchema);
---
+++ utils/ticketTypes.js
@@ -0,0 +1,8 @@
+const TicketTypes = Object.freeze({
+    GENERAL_TICKET: 'GENERAL_TICKET',
+    VIP_TICKET: 'VIP_TICKET',
+    EARLY_BIRD_TICKET: 'EARLY_BIRD_TICKET',
+    GROUP_TICKET: 'GROUP_TICKET',
+});
+
+module.exports = TicketTypes;
